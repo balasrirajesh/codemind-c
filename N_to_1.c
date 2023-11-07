7432b9abf1f25ad7dbfdc53125649e1d2beec422
balasrirajesh <147487671+balasrirajesh@users.noreply.github.com>
2023-11-07 20:20:20 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d",&a);     for(c=a;c<=a;c--)     {         if(c==0)         {             break;         }         printf("%d ",c);     } }

---
+++ N_to_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    for(c=a;c<=a;c--)
+    {
+        if(c==0)
+        {
+            break;
+        }
+        printf("%d ",c);
+    }
+}
