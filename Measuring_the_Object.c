5d77e059473e7935ddb11b488e4b49a42796b5e5
balasrirajesh <147487671+balasrirajesh@users.noreply.github.com>
2023-11-22 15:25:31 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d%d%d%d",&a,&b,&c,&d);     if(a==b || a==c || a==d || a==b+c || a==c+d || a==b+d)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Measuring_the_Object.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if(a==b || a==c || a==d || a==b+c || a==c+d || a==b+d)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
