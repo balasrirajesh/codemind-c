1b25022f507910eecb0ac7886acf40b8a1e81874
balasrirajesh <147487671+balasrirajesh@users.noreply.github.com>
2023-12-18 15:22:28 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,a=0;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(i%2==0)         {             a=a+arr[i];         }     }     printf("%d",a); }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,a=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(i%2==0)
+        {
+            a=a+arr[i];
+        }
+    }
+    printf("%d",a);
+}
