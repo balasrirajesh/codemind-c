5fed3d053ab4ba41b11140420e23841e471b704e
balasrirajesh <147487671+balasrirajesh@users.noreply.github.com>
2023-12-18 15:04:23 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,a=0;     for(i=0;i<n-1;i++)     {         scanf("%d",&arr[i]);         if(arr[i]%2!=0)         {             a=a+arr[i];         }     }     printf("%d",i); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,a=0;
+    for(i=0;i<n-1;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]%2!=0)
+        {
+            a=a+arr[i];
+        }
+    }
+    printf("%d",i);
+}
